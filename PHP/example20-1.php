f91f36efad2914100d09e43ae564705bb4972c23
OFF <peerapat13th@gmail.com>
2024-03-27 16:23:07 +0700

    php: example20-1

---
+++ PHP/example20-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <?php
+    $q = 'php';
+    $b = 1;
+    $n = 10;
+    $t = 1000;
+    echo '
+        <b> Single quote using</b><br>
+        Find: $q result order $b - $n from $t    
+    ';
+        echo '<br><br>';
+        echo "
+            <b> Single quote using</b><br>
+            Find: $q result order $b - $n from $t   
+        ";
+    ?>
+    
+</body>
+</html>
