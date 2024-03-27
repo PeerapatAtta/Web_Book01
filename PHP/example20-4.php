07dbbc44faeea2cd0eff5411398aa509c873757e
OFF <peerapat13th@gmail.com>
2024-03-27 22:05:47 +0700

    example20-4

---
+++ PHP/example20-4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <?php
+        $k = 1;
+        $m = 0;
+        while ($k < 10){
+            $m = $k*0.6;
+            echo "$k km = $m mile <br>";
+            $k++;
+        }
+    ?>
+    
+</body>
+</html>
