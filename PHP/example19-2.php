1bb9ad05c6a7c585ec31100342ec36c2af626e8d
OFF <peerapat13th@gmail.com>
2024-03-27 16:13:21 +0700

    Start project for PHP

---
+++ PHP/example19-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <h3><?php echo 'PHP for VS Code'; ?></h3>
+    <div style="font-size: 12pt; color:blue">
+        <?php echo 'Installation ' ?>
+    </div>
+    <ul>
+        <?php
+        echo '<li>PHP Interpreter</li>';
+        echo '<li>Intelephense</li>';
+        echo '<li>PHP Server</li>';
+        ?>
+    </ul>
+
+
+</body>
+
+</html>
